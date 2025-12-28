c5c3586aa0a932177596d6700582d2cd30bba608
RieraDiPe <rieradipe@gmail.com>
2025-12-28 12:48:31 +0100

    Adaptado index.js para Render y variables de entorno

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import app from "./app.js";
 import logger from "./logger/index.js";
 
 const PORT = process.env.PORT || 7070;
+const API_URL = process.env.API_URL || `http://localhost:${PORT}`;
 
 process.on("uncaughtException", (err) => {
   logger.fatal({ err }, "uncaught_exception");
@@ -12,10 +13,13 @@ process.on("uncaughtException", (err) => {
 process.on("unhandledRejection", (reason) => {
   logger.fatal({ reason }, "unhandled_rejection");
 });
-console.log("JWT_SECRET =", process.env.JWT_SECRET);
-console.log("ADMIN_USER =", process.env.ADMIN_USER);
-console.log("ADMIN_HASHED_PASSWORD =", process.env.ADMIN_HASHED_PASSWORD);
+
+if (process.env.NODE_ENV !== "production") {
+  console.log("JWT_SECRET =", process.env.JWT_SECRET);
+  console.log("ADMIN_USER =", process.env.ADMIN_USER);
+  console.log("ADMIN_HASHED_PASSWORD =", process.env.ADMIN_HASHED_PASSWORD);
+}
 
 app.listen(PORT, () => {
-  logger.info(`🚀 API en http://localhost:${PORT}`);
+  logger.info(`🚀 API corriendo en ${API_URL}`);
 });

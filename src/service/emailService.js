35f125f46353af3f1db034280670b18a47d1a9d7
RieraDiPe <rieradipe@gmail.com>
2026-04-08 22:17:46 +0200

    Mejoramos la firma de gmail

--- src/service/emailService.js
+++ src/service/emailService.js
@@ -3,47 +3,58 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-// Tu firma HTML (la mantenemos 💖)
+// ✨ FIRMA HTML (mejorada con enlaces clicables)
 const firmaHTML = `
 <div dir="ltr">
-  <table cellpadding="0" cellspacing="0" style="font-family:sans-serif;color:rgb(33,33,33);padding:10px">
+  <table cellpadding="0" cellspacing="0" style="font-family:sans-serif;color:#212121;padding:10px">
     <tbody>
       <tr>
         <td style="padding-right:20px;vertical-align:top">
-          <img src="https://i.imgur.com/gu0mLBz.png" alt="Logo" width="90" height="90" style="border-radius:8px">
+          <img src="https://i.imgur.com/gu0mLBz.png" alt="Logo RieraDipe" width="90" height="90" style="border-radius:8px">
         </td>
         <td style="vertical-align:top">
           <table cellpadding="0" cellspacing="0" style="margin-bottom:10px">
             <tbody>
               <tr>
-                <td style="font-weight:bold;font-size:18px;color:rgb(191,103,58);line-height:1.2;border-bottom:3px solid rgb(66,54,54);padding-bottom:2px">
+                <td style="font-weight:bold;font-size:18px;color:#bf673a;line-height:1.2;border-bottom:3px solid #423636;padding-bottom:2px">
                   Alba Riera
                 </td>
               </tr>
               <tr>
-                <td style="font-size:15px;font-weight:600;color:rgb(66,54,54);padding-top:5px">
-                  Fundadora &amp; Desarrolladora – RieraDipe
+                <td style="font-size:15px;font-weight:600;color:#423636;padding-top:5px">
+                  Fundadora & Desarrolladora – RieraDipe
                 </td>
               </tr>
             </tbody>
           </table>
+
           <table cellpadding="0" cellspacing="0" style="font-size:13px;line-height:1.4;text-align:center">
             <tbody>
               <tr>
                 <td style="padding-right:20px">
-                  📧 <a href="mailto:contacto@rieradipe.dev" style="color:rgb(66,54,54)" target="_blank">contacto@rieradipe.dev</a>
+                  📧 
+                  <a href="mailto:contacto@rieradipe.dev" style="color:#423636;text-decoration:none">
+                    contacto@rieradipe.dev
+                  </a>
                 </td>
                 <td style="padding-left:20px">
-                  📞 <span style="color:rgb(66,54,54)">632193202</span>
+                  📞 
+                  <a href="tel:+34632193202" style="color:#423636;text-decoration:none">
+                    632193202
+                  </a>
                 </td>
               </tr>
               <tr>
                 <td colspan="2" style="padding-top:5px">
-                  🌐 <span style="color:rgb(66,54,54)">https://rieradipe.dev</span>
+                  🌐 
+                  <a href="https://rieradipe.dev" style="color:#423636;text-decoration:none" target="_blank">
+                    rieradipe.dev
+                  </a>
                 </td>
               </tr>
             </tbody>
           </table>
+
         </td>
       </tr>
     </tbody>
@@ -66,7 +77,7 @@ export const sendContactMail = async ({ nombre, email, asunto, mensaje }) => {
   return await resend.emails.send({
     from: `RieraDipe <${process.env.CONTACT_FROM_EMAIL}>`,
     to: [process.env.CONTACT_TO_EMAIL],
-    replyTo: email, // 🔥 clave para responder al usuario
+    reply_to: email, // 🔥 IMPORTANTE (nombre correcto en Resend)
     subject: `[Contacto] ${asunto}`,
     html: htmlContent,
   });
